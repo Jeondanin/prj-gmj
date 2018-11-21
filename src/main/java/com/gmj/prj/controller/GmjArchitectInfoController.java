8b78207b2a3fe4e931d0c8282103bdddbe957464
KOITT <KOITT@KOITT-PC>
2018-11-21 17:59:41 +0900

    111

--- src/main/java/com/gmj/prj/controller/GmjArchitectInfoController.java
+++ src/main/java/com/gmj/prj/controller/GmjArchitectInfoController.java
@@ -10,20 +10,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.gmj.prj.aop.AspectLogger;
 import com.gmj.prj.service.GmjArchitectInfoService;
 import com.gmj.prj.service.GmjFavoriteService;
-import com.gmj.prj.service.GmjMapService;
-import com.gmj.prj.service.impl.GmjArchitectInfoServiceImpl;
-import com.gmj.prj.service.impl.GmjFavoriteServiceImpl;
+
 import com.gmj.prj.vo.GmjArchitectInfo;
 import com.gmj.prj.vo.GmjFavorite;
 
